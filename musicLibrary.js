4dfce0dd7c7c5e2217635092c5813c61862883e7
Daniel Christine <danieljameschristine@gmail.com>
2020-09-10 19:05:17 -0500

    Init

---
+++ musicLibrary.js
@@ -0,0 +1,11 @@
+"use strict"
+
+
+
+function getAllMusic(){
+    //gets array of JSON objects from DB
+    return [];
+}
+function getSpecficMusic(id){
+    //gets specific music object with this id
+    return {}
