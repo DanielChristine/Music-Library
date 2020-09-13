85190209a02b10134f9d9a4df322d6f40be2ce27
Daniel Christine <danieljameschristine@gmail.com>
2020-09-13 12:45:42 -0500

    Table built and information properly imported.

--- musicLibrary.js
+++ musicLibrary.js
@@ -1,21 +1,46 @@
-"use strict"
+"use strict";
 
-function get(){
-$.get("http://www.devcodecampmusiclibrary.com/api/music", function(response, status){
+function get() { // get vs getJSON
+  $.get("http://www.devcodecampmusiclibrary.com/api/music", function (
+    response,
+    status
+  ) {
+    writeTable(response); // should status be added as a parameter to writeTable
+  });
+}
 
-writeTable(response);
-
-})}
-
-function writeTable(response){
-    for (let index = 0; index < response.length; index++) {
-$("tbody").append(`<td>${0}</td>`);
-    }
+function writeTable(response) {
+  for (let index = 0; index < response.length; index++) {
+    $("tbody").append(`<tr>
+                            <td>${response[index]["id"]}</td>
+                            <td>${response[index]["title"]}</td>
+                            <td>${response[index]["album"]}</td>
+                            <td>${response[index]["artist"]}</td>
+                            <td>${response[index]["genre"]}</td>
+                            <td>${response[index]["releaseDate"]}</td>
+                      </tr>`); 
+  }
 }
 
 get();
 
 
-function getSpecficMusic(id){
 
-    return {}
+// $.ajax({
+//     url: `http://www.devcodecampmusiclibrary.com/api/music`,
+//     dataType: "json",
+//     type: "post",
+//     success: function (data, textStatus, jQxhr) {
+//       //Do something with response data (data parameter)
+//     },
+//     error: function (jqXhr, textStatus, errorThrown) {
+//       //if the request fails, the errorThrown parameter will contain details of the error
+//       console.log(errorThrown);
+//     },
+//   });
+
+// let findMusic = prompt("Enter an artist, album, title, etc.")
+// function getSpecficMusic(id){
+
+//     return;
+// }
